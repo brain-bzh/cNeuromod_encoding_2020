477de69293489ad03400a7a676d6da8dcc7475c4
maelleF <maelle.freteault@gmail.com>
2020-12-11 22:27:55 +0100

    Visualisation Best ROI - WIP

--- visualisation_utils.py
+++ visualisation_utils.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 import pickle
 from torch import load
 
@@ -10,8 +11,10 @@ from nilearn.regions import signals_to_img_labels
 from matplotlib import pyplot as plt 
 
 
+ROI_info = '/home/maelle/Database/MIST_parcellation/Parcel_Information/MIST_ROI.csv'
 data_path = '/home/maelle/Results/encoding_12_2020/'
 out_directory = '/home/maelle/Results/encoding_12_2020/analysis'
+
 
 def plot_train_val_data(subject, data_films, measure, colors = ['b', 'g', 'm', 'r']) : 
     f = plt.figure()
@@ -59,7 +62,40 @@ for sub, films in all_maps.items():
     all_loaded = [(dir_name, image.load_img(file_path)) for (dir_name, file_path) in films]
     all_maps[sub] = all_loaded
 
-for sub, data in all_data.items():
+
+df = pd.read_csv(ROI_info, sep=';', index_col=0)
+print(df['name'][1])
+
+n = 10
+all_index = []
+for sub, films_data in all_data.items():
+    for (film, data) in films_data:
+        r2_by_ROI = data['r2']
+        best_index = np.flip(np.argsort(r2_by_ROI))[:n]
+        best_index += 1
+        all_index.extend(list(best_index))
+
+indexes = set(all_index)
+print(indexes)
+labels_ROI = {}
+for index in indexes:
+    labels_ROI[index] = df['name'][index]
+
+for index, roi in labels_ROI.items():
+    print(index, roi)
+
+for sub, films_data in all_data.items():
+    for (film, data) in films_data:
+        r2_by_ROI = data['r2']
+        best_index = np.flip(np.argsort(r2_by_ROI))[:n]
+        best_index += 1
+        print(best_index, sub, film)
+
+
+
+
+
+#for sub, data in all_data.items():
     #plot_train_val_data(sub, data, "loss")
     #plot_train_val_data(sub, data, "r2_max")
     #plot_train_val_data(sub, data, "r2_mean")
