small fixes + normalization file

models/encoding_models.py
@@ -79,12 +79,12 @@ class SoundNetEncoding(nn.Module):
         return out
 
 class SoundNetEncoding_conv(nn.Module):
-    def __init__(self,pytorch_param_path,nroi,fmrihidden=1000, kernel_size = 1, nroi_attention=None, hrf_model=None, oversampling = 16, tr = 1.49, audiopad = 0,transfer=True,preload=True):
+    def __init__(self,pytorch_param_path,out_size,fmrihidden=1000, kernel_size = 1, nroi_attention=None, hrf_model=None, oversampling = 16, tr = 1.49, audiopad = 0,transfer=True,preload=True):
         super(SoundNetEncoding_conv, self).__init__()
 
         self.soundnet = snd.SoundNet8_pytorch()
         self.fmrihidden = fmrihidden
-        self.nroi = nroi
+        self.out_size = out_size
 
         if preload:
             print("Loading SoundNet weights...")
@@ -100,14 +100,14 @@ class SoundNetEncoding_conv(nn.Module):
                 print("Finetuning : backbone will be optimized")
 
         self.encoding_fmri = nn.Sequential(                
-                nn.Conv1d(1024,self.nroi,kernel_size=(kernel_size,1), padding=(kernel_size-1,0)),
+                nn.Conv1d(1024,self.out_size,kernel_size=(kernel_size,1), padding=(kernel_size-1,0)),
                 #nn.ReLU(inplace=True),
-                #nn.Conv2d(self.fmrihidden,self.nroi,kernel_size=(1,1)),
+                #nn.Conv2d(self.fmrihidden,self.out_size,kernel_size=(1,1)),
 
             )
 
         if nroi_attention is not None:
-            self.maskattention = torch.nn.Parameter(torch.rand(nroi,nroi_attention))
+            self.maskattention = torch.nn.Parameter(torch.rand(out_size,nroi_attention))
         else:
             self.maskattention = None
         