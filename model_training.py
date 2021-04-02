d53d02997fa2a9b051bab0e2adc7626560d6c649
maelleF <maelle.freteault@gmail.com>
2021-04-03 01:58:23 +0200

    added bash command option for unitary training

--- model_training.py
+++ model_training.py
@@ -1,14 +1,11 @@
 #Generic
-import os
-import warnings
+import os, warnings, argparse
 import numpy as np
 from datetime import datetime
 #Get input & preprocess
-from files_utils import associate_stimuli_with_Parcellation, create_dir_if_needed, print_dict
-from audio_utils import convert_Audio
+import files_utils as fu
 #Create Dataset
-from itertools import islice
-from random import shuffle, sample
+from random import sample
 from torch.utils.data import DataLoader
 from Datasets_utils import SequentialDataset, create_usable_audiofmri_datasets
 #Models
@@ -22,7 +19,7 @@ from nilearn.plotting import plot_stat_map
 from nilearn.regions import signals_to_img_labels
 from matplotlib import pyplot as plt 
 
-def main_model_training(outpath, data_selection, data_processing, training_hyperparameters):
+def model_training(outpath, data_selection, data_processing, training_hyperparameters):
     checkpt_still_here = os.path.lexists('checkpoint.pt')
     if checkpt_still_here : 
         print('suppression of checkpoint file')
@@ -45,6 +42,8 @@ def main_model_training(outpath, data_selection, data_processing, training_hyper
     fmrihidden = training_hyperparameters['fmrihidden']
     kernel_size = training_hyperparameters['kernel_size']
     gpu = training_hyperparameters['gpu']
+    print(gpu)
+    print(type(gpu))
     batchsize = training_hyperparameters['batchsize']
     lr = training_hyperparameters['lr']
     weight_decay = training_hyperparameters['weight_decay']
@@ -60,13 +59,14 @@ def main_model_training(outpath, data_selection, data_processing, training_hyper
     #problem, that stop the training of any following test
     #to look how to implement a more interactable early_stopping
 
-    outfile_name = str(scale)+str(nroi)+'_'+str(model.__name__)+'_'+str(fmrihidden)+'_ks'+str(kernel_size)+'_lr'+str(lr)+'_wd'+str(weight_decay)+'_'
+    outfile_name = str(film)+'_'+str(scale)+str(nroi)+'_'+str(model.__name__)+'_'+str(fmrihidden)+'_ks'+str(kernel_size)+'_lr'+str(lr)+'_wd'+str(weight_decay)+'_'
     outfile_name = outfile_name+'decoupled_' if decoupled_weightDecay else outfile_name
     outfile_name = outfile_name+'pt_' if power_transform else outfile_name
     destdir = outpath
 
     #-------------------------------------------------------------
-    all_data = all_subs_files[subject][film]
+    id_sub = subject-1
+    all_data = all_subs_files[id_sub][film]
 
     DataTest = []
     DataTrain = []
@@ -76,10 +76,13 @@ def main_model_training(outpath, data_selection, data_processing, training_hyper
         if len(fmri_sesvid)>1 and diff_TrainTest:
             DataTrain.append((wav, fmri_sesvid[0]))
             DataTest.append((wav, fmri_sesvid[1]))
-        else : 
+        elif wav.find('s2e') > -1:
+            DataTest.append((wav, fmri_sesvid[0]))
+        elif wav.find('s1e') : 
             DataTrain.append((wav, fmri_sesvid[0]))
 
-    print(len(DataTest))
+    print(f'lenght of DataTrain : ', len(DataTrain))
+    print(f'lenght of DataTest : ', len(DataTest))
 
     xTrain, yTrain = create_usable_audiofmri_datasets(DataTrain, tr, sr, name='training')
     TrainDataset = SequentialDataset(xTrain, yTrain, batch_size=batchsize, selection=selected_ROI)
@@ -153,7 +156,7 @@ def main_model_training(outpath, data_selection, data_processing, training_hyper
             # early_stopping needs the R2 mean to check if it has increased, 
             # and if it has, it will make a checkpoint of the current model
             
-            r2_forEL = -(val_r2_max[-1])
+            #r2_forEL = -(val_r2_max[-1])
             early_stopping(t_l, net)
 
             if early_stopping.early_stop:
@@ -214,3 +217,87 @@ def main_model_training(outpath, data_selection, data_processing, training_hyper
     if checkpt_still_here : 
         print('suppression of checkpoint file')
         os.remove('checkpoint.pt')
+
+#training_called_by_bash
+if __name__ == "__main__":
+    date = datetime.now()
+    dt_string = date.strftime("%Y%m%d")
+    #bash command example : python  model_training.py -s 1 -f friends --scale MIST_ROI --nroi 210 --train100 1.0 --test100 0.5 --val100 0.5
+    parser = argparse.ArgumentParser()
+    #data_selection
+    parser.add_argument("-s", "--sub", type=int)
+    parser.add_argument("-f", "--film", type=str)
+    parser.add_argument("--sessions", type=str, nargs='+', default=None)
+    #data_processing
+    parser.add_argument("--scale", type=str)
+    parser.add_argument("--tr", type=float, default=1.49)
+    parser.add_argument("--sr", type=int, default=22050)
+    parser.add_argument("--nroi", type=int)
+    parser.add_argument("--select", type=int, nargs='+', default=None)
+    #training_hyperparameters
+    parser.add_argument("--hidden_size", type=int, default=1000)
+    parser.add_argument("--bs", type=int, default=30)
+    parser.add_argument("--ks", type=int, default=5)
+    parser.add_argument("--train100", type=float)
+    parser.add_argument("--test100", type=float)
+    parser.add_argument("--val100", type=float)
+    parser.add_argument("--gpu", dest='gpu', action='store_true')
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--nbepoch", type=int, default=200)
+    parser.add_argument("--wd", type=float, default=1e-2)
+    parser.add_argument("--decoupledWD", dest='decoupledWD', action='store_true')
+    parser.add_argument("--powerTransform", dest='powerTransform', action='store_true')
+
+    args = parser.parse_args()
+    data_selection = {
+        'subject':args.sub,
+        'film':args.film,
+        'sessions':args.sessions
+    }
+    ds = data_selection
+
+    data_processing = {
+        'scale': args.scale,
+        'tr' : args.tr,
+        'sr' : args.sr,
+        'selected_ROI': args.select,
+        'nroi': args.nroi                           
+    }
+    dp = data_processing
+
+    training_hyperparameters = {
+        'model':encod.SoundNetEncoding_conv,
+        'diff_sess_for_train_test':True,
+        'mseloss':nn.MSELoss(reduction='sum'),
+
+        'fmrihidden':args.hidden_size,
+        'batchsize':args.bs,
+        'kernel_size':args.ks,
+        'train_percent':args.train100,
+        'test_percent':args.test100,
+        'val_percent':args.val100,
+        'gpu':args.gpu,
+        'lr':args.lr,
+        'nbepoch': args.nbepoch,
+        'weight_decay':args.wd,
+        'decoupled_weightDecay' : args.decoupledWD,
+        'power_transform' : args.powerTransform
+    }
+    th = training_hyperparameters
+
+    outpath = '/home/maelle/Results' #"/home/maellef/Results/"
+    stimuli_path = '/home/maelle/DataBase/stimuli' #'/home/maellef/DataBase/stimuli'
+    #stimuli_outpath = 'home/maellef/DataBase/stimuli/friends'
+    os.makedirs(stimuli_path, exist_ok=True)
+
+    path_embed = '/home/maelle/DataBase/fMRI_Embeddings/Movie10/embed_2020_norm' #'/home/maellef/DataBase/fMRI_Embeddings/Friends/embed_2021_norm'
+    embed_used = os.path.basename(path_embed).replace('_', '')
+    path_parcellation = os.path.join(path_embed, dp['scale'])
+    all_subs_files = fu.associate_stimuli_with_Parcellation(stimuli_path, path_parcellation)
+    resultpath = outpath+dt_string+"_generalisation_btw_s1_s2_{}{}_lr0.01_{}epochs_{}".format(dp['scale'],dp['nroi'], th['nbepoch'],embed_used)
+    resultpath = os.path.join(resultpath, 'sub-0'+str(ds['subject']))
+    os.makedirs(resultpath, exist_ok=True)
+    
+    ds['all_data']=all_subs_files
+    model_training(resultpath, ds, dp, th)
+
