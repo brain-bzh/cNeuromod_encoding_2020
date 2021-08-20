c96b574a7565c266386021b662c1ef0e810e7315
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2021-08-20 11:15:20 +0200

    added arguments for segment length, step and padding, and default values and results in help string

--- baselin_ESC50.py
+++ baselin_ESC50.py
@@ -20,13 +20,16 @@ from models import soundnet_model as snd
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--esc", type=str,default="/home/maelle/DataBase/ESC-50-master/",help="Path to the ESC-50 folder (must be cloned from https://github.com/karolpiczak/ESC-50 )")
+parser = argparse.ArgumentParser(epilog='With all default parameters, you should obtain average scores accross folds for train:  0.9914583333333333 , and test :  0.8125',
+formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--esc", type=str,default="/home/maelle/DataBase/ESC-50-master/",help="Path to the ESC-50 folder (must be cloned from https://github.com/karolpiczak/ESC-50)")
 parser.add_argument("--checkpoint","-c", type=str,default='./sound8.pth',help="Path to soundnet checkpoint to load, default is the trained soundnet from the paper")
 parser.add_argument("--dataset", type=str,default='esc10',choices=['esc10','esc50'],help="esc10 or esc50")
 parser.add_argument("--layer", type=str,default='conv6',choices=['conv1', 'pool1', 'conv2', 'pool2' , 'conv3' , 'conv4' , 
                             'conv5', 'pool5', 'conv6', 'conv7'],help="Choose soundnet layer to train from")
-
+parser.add_argument("--seg", type=float,default='3.0',help="Segment length (s) to divide the wav files ")
+parser.add_argument("--step", type=float,default='1.0',help="Step length (s) between segments")
+parser.add_argument("--padding", type=float,default='5.0',help="How much padding (s) to add before and after the segments")
 parser.add_argument("--classif", type=str,default='mlp',choices=['svm', 'mlp', 'knn'],help="Choose classifer (SVM, MLP, KNN)")
 
 args = parser.parse_args()
@@ -90,16 +93,16 @@ for filename, target, fold in tqdm(zip(dataset['filename'], dataset['category'],
     ### Setting dur_sample to 5 seconds disables DA
 
     total_tp = len(x)
-    dur_sample = int(sr_soundnet*3)#nb de sample pour chaque exemple
-    overlap = 3
-    step = int(dur_sample/overlap)
+    dur_sample = int(sr_soundnet*args.seg)#nb de sample pour chaque exemple
+    
+    step = int(sr_soundnet*args.step)
 
     for i, start in enumerate(range(0, total_tp, step)) : 
         sub_x = x[start:start+dur_sample]
         
         if len(sub_x) == dur_sample : 
             ### padding by copying the first and final sample many times at the beginning and end
-            pad_length = 5 * sr_soundnet ### for example 8 * sr_soundnet will add 8 seconds at the beginning and end  
+            pad_length = int(args.padding * sr_soundnet) ### for example 8 * sr_soundnet will add 8 seconds at the beginning and end  
             pad_beg = np.ones_like(sub_x)[:pad_length]
             pad_end = np.ones_like(sub_x)[:pad_length]
             sub_x = np.hstack([pad_beg,sub_x,pad_end])
