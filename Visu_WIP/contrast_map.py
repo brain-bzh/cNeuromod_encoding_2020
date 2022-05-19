341e4dea96d69fdcbf2e2bf281c716722f29fbc5
maelleF <maelle.freteault@gmail.com>
2022-05-19 19:04:46 -0400

    differents scripts for vsu/figs

--- Visu_WIP/contrast_map.py
+++ Visu_WIP/contrast_map.py
@@ -1,6 +1,7 @@
 from nilearn import datasets, surface, plotting, regions, image, input_data
 import os
 import matplotlib.pyplot as plt
+from visu_utils import brain_3D_map
 
 outpath = '/home/maelle/Results/figures_finefriends2'
 os.makedirs(outpath, exist_ok=True)
@@ -68,23 +69,24 @@ voxels = [
     'sub06' : './nii/sub06_finetune_conv4_auditory_voxels_r2_map.nii'
 }]
 
-def brain_3D_map(stat_img, title='', hemisphere='right', threshold=1.0, figure=None, output_file=None):
-    fsaverage = datasets.fetch_surf_fsaverage()
-    texture = surface.vol_to_surf(stat_img, fsaverage.pial_right)
-    plotting.plot_surf_stat_map(fsaverage.infl_right, texture, hemi=hemisphere,title=title, colorbar=True,threshold=threshold, bg_map=fsaverage.sulc_right,output_file=output_file, figure=figure)
-
-for i, scale in zip(['roi'], [roi]):
+for scale_name, scale in zip(['roi'], [roi]):
     database_noft = scale[0]
     database_ft = scale[1:]
-    for dicto, nb_conv in zip(database_ft, [7,6, 5, 4]) : 
+    for dicto, nb_conv in zip(database_ft, [7,6,5,4]) : 
         for (sub, base_path), (sub, conv_path) in zip(database_noft.items(), dicto.items()):
             bp = image.load_img(base_path)
             conv = image.load_img(conv_path)
-            contrast_img = image.math_img("conv - bp", conv=conv, bp=bp)
-            out_directory = os.path.join(outpath, '{}_{}_{}'.format(sub, i, nb_conv))
+
+            conv_no0 = image.math_img("conv+(1**-10000)", conv=conv) 
+            bp_no0 = image.math_img("bp+(1**-10000)", bp=bp)
+            contrast_img = image.math_img("(conv - bp)*100/(bp)", conv=conv_no0, bp=bp_no0)
+            
+            out_directory = os.path.join(outpath, '{}_contrast_{}_{}'.format(sub, scale_name, nb_conv))
             f = plt.figure(figsize=(8.0,5.5))
-            if i == 'roi':
-                plotting.plot_stat_map(conv, display_mode='z', cut_coords=[-11, 4, 16, 28, 40], figure=f, colorbar=True, symmetric_cbar=True, threshold=0.05, vmax=0.4)
+            
+            
+            if scale_name == 'roi':
+                plotting.plot_stat_map(contrast_img, display_mode='z', cut_coords=[-11, 4, 16, 28, 40], figure=f, colorbar=True, symmetric_cbar=True, threshold=0, vmax=8)
             else :
                 brain_3D_map(contrast_img, hemisphere='right', threshold=0.05, output_file=os.path.join(out_directory))
             f.savefig(out_directory)
